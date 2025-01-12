869c9327de63eeb9f82015d39ff37d048d6e60c1
Renaldis <renaldis142@gmail.com>
2025-01-12 19:34:28 +0700

    fix(resume): fix key in a list children prop

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,4 +1,8 @@
-import { workingHistory, educationHistory } from "../../utils/resumeDate";
+import {
+  workingHistory,
+  educationHistory,
+  nonFormalEducation,
+} from "../../utils/resumeDate";
 
 export default function Resume() {
   return (
@@ -7,46 +11,11 @@ export default function Resume() {
       className="bg-white shadow-sm rounded-lg border border-slate-100"
     >
       <div className="p-5">
-        <h1 className="mb-5">Resume</h1>
-        <div className="grid grid-cols-2">
-          <div className="relative">
-            <ol className="left-5 relative border-s-2 border-gray-200 dark:border-gray-700">
-              <li className="mb-8 ms-4 flex items-center">
-                <div className="absolute -start-5 flex items-center justify-center w-10 h-10 bg-yellow-300 rounded-full border border-yellow-300 shadow-md">
-                  <i className="fas fa-shopping-bag" />
-                </div>
-                <span className="ms-3 font-semibold">Experience</span>
-              </li>
-              {workingHistory.map((item) => {
-                return (
-                  <li className="mb-2 ms-4 text-sm">
-                    <div className="absolute w-3 h-3 bg-yellow-200 rounded-full mt-1.5 -start-[7.6px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                    <div className="flex items-center gap-2">
-                      <div className="overflow-hidden rounded-full w-14 bg-blue-600">
-                        <img
-                          src={item.img}
-                          alt={item.company}
-                          className="w-full"
-                        />
-                      </div>
-                      <h1 className="font-semibold">{item.title}</h1>
-                    </div>
-                    <div className="flex flex-col">
-                      <span className="text-slate-500">{item.date}</span>
-                      <span className="text-slate-500">
-                        {item.location} | {item.locType}
-                      </span>
-                    </div>
-                    <ul>
-                      {item.description.map((desc) => {
-                        return <li>- {desc}</li>;
-                      })}
-                    </ul>
-                  </li>
-                );
-              })}
-            </ol>
-          </div>
+        <h1 className="mb-5 border-b-2 border-yellow-200 inline-block font-semibold">
+          Resume
+        </h1>
+        <div className="grid grid-cols-2 border-b border-gray-200">
+          {/* Formal Education */}
           <div className="relative">
             <ol className="left-5 relative border-s-2 border-gray-200 dark:border-gray-700">
               <li className="mb-8 ms-4 flex items-center">
@@ -55,15 +24,15 @@ export default function Resume() {
                 </div>
                 <span className="ms-3 font-semibold">Formal Education</span>
               </li>
-              {educationHistory.map((item) => {
-                return (
-                  <li className="mb-2 ms-4 text-sm">
+              {educationHistory?.length ? (
+                educationHistory.map((item, idx) => (
+                  <li key={idx} className="mb-2 ms-4 text-sm">
                     <div className="absolute w-3 h-3 bg-yellow-200 rounded-full mt-1.5 -start-[7.6px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <div className="flex items-center gap-2">
                       <div className="overflow-hidden rounded-full w-20">
                         <img
                           src={item.img}
-                          alt={item.title}
+                          alt={`Institution Logo of ${item.title}`}
                           className="w-full"
                         />
                       </div>
@@ -75,17 +44,107 @@ export default function Resume() {
                     </div>
                     <h1 className="text-slate-700">Learn:</h1>
                     <ul className="list-disc ms-4">
-                      {item.description.map((desc) => {
-                        return (
-                          <div className="flex flex-col text-slate-800 cursor-pointer">
-                            <li>{desc.title}</li>
-                          </div>
-                        );
-                      })}
+                      {item.description.map((desc, idx) => (
+                        <li key={idx} className="text-slate-800">
+                          {desc.title}
+                        </li>
+                      ))}
                     </ul>
                   </li>
-                );
-              })}
+                ))
+              ) : (
+                <p>No education history available.</p>
+              )}
+            </ol>
+          </div>
+          {/* Working History */}
+          <div className="relative">
+            <ol className="left-5 relative border-s-2 border-gray-200 dark:border-gray-700">
+              <li className="mb-8 ms-4 flex items-center">
+                <div className="absolute -start-5 flex items-center justify-center w-10 h-10 bg-yellow-300 rounded-full border border-yellow-300 shadow-md">
+                  <i className="fas fa-shopping-bag" />
+                </div>
+                <span className="ms-3 font-semibold">Experience</span>
+              </li>
+              {workingHistory?.length ? (
+                workingHistory.map((item, idx) => (
+                  <li key={idx} className="mb-2 ms-4 text-sm">
+                    <div className="absolute w-3 h-3 bg-yellow-200 rounded-full mt-1.5 -start-[7.6px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex items-center gap-2">
+                      <div className="overflow-hidden rounded-full w-14 bg-blue-600">
+                        <img
+                          src={item.img}
+                          alt={`Logo of ${item.company}`}
+                          className="w-full"
+                        />
+                      </div>
+                      <h1 className="font-semibold">{item.title}</h1>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-slate-500">{item.date}</span>
+                      <span className="text-slate-500">
+                        {item.location} | {item.locType}
+                      </span>
+                    </div>
+                    <ul className="list-disc ms-4">
+                      {item.description.map((desc, idx) => (
+                        <li key={idx} className="text-slate-800">
+                          {desc}
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))
+              ) : (
+                <p>No working history available.</p>
+              )}
+            </ol>
+          </div>
+        </div>
+
+        {/* Non-Formal Education */}
+        <div className="grid mt-10">
+          <div className="relative">
+            <ol className="left-5 relative border-s-2 border-gray-200 dark:border-gray-700">
+              <li className="mb-8 ms-4 flex items-center">
+                <div className="absolute -start-5 flex items-center justify-center w-10 h-10 bg-yellow-300 rounded-full border border-yellow-300 shadow-md">
+                  <i className="fas fa-graduation-cap" />
+                </div>
+                <span className="ms-3 font-semibold">Non-Formal Education</span>
+              </li>
+              {nonFormalEducation?.length ? (
+                nonFormalEducation.map((item, idx) => (
+                  <li key={idx} className="mb-2 ms-4 text-sm">
+                    <div className="absolute w-3 h-3 bg-yellow-200 rounded-full mt-1.5 -start-[7.6px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex items-center gap-2">
+                      <div className="overflow-hidden rounded-full w-20">
+                        <img
+                          src={item.img}
+                          alt={`Institution Logo of ${item.title}`}
+                          className="w-full"
+                        />
+                      </div>
+                    </div>
+                    <h1 className="font-semibold">{item.title}</h1>
+                    <div className="flex flex-col">
+                      <span className="text-slate-500">
+                        {item.type} - {item.location}
+                      </span>
+                      <span className="text-slate-500">{item.date}</span>
+                    </div>
+                    <h1 className="text-slate-700">Learn:</h1>
+                    <ul className="list-disc ms-4">
+                      {item.description.map((desc, idx) => (
+                        <li key={idx} className="text-slate-800">
+                          {desc}
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))
+              ) : (
+                <p>No non-formal education available.</p>
+              )}
             </ol>
           </div>
         </div>
--- src/utils/resumeDate.js
+++ src/utils/resumeDate.js
@@ -8,7 +8,12 @@ let workingHistory = [
     locType: "on-site",
     description: [
       "Helping instructor during courses",
-      "inputting the grades of practitioners who attended the course",
+      "Inputting the grades of practitioners who attended the course",
+      "Supervising and assisting students in understanding intermediate-level C# and SQL materials",
+      "Providing guidance to students in solving practical exercises and debugging their code",
+      "Preparing and organizing course materials to support effective learning",
+      "Offering one-on-one support for students who required additional assistance",
+      "Ensuring smooth operation of the class by coordinating with instructors and handling technical issues during sessions",
     ],
     img: "https://media.licdn.com/dms/image/v2/C510BAQFZuJ22U-5ECw/company-logo_100_100/company-logo_100_100/0/1630580476989?e=1744848000&v=beta&t=e5TKH8gQ0gDallZ0GpeImKav9TMorxwp64A0xmmHXkA",
   },
@@ -70,4 +75,28 @@ let educationHistory = [
   },
 ];
 
-export { workingHistory, educationHistory };
+let nonFormalEducation = [
+  {
+    id: 1,
+    title: "Learning Frontend ReactJS",
+    employe: "sanbercode",
+    type: "Bootcamp",
+    img: "https://sanbercode.com/logo-horizontal.png",
+    date: "2 Desember - 27 Desember",
+    location: "online",
+    description: [
+      "Introduction to ReactJS",
+      "Components & Props",
+      "State & Component Lifecycle",
+      "Lists & Forms",
+      "Hooks",
+      "REST API with Axios",
+      "Context",
+      "React Router",
+      "Template UI (Design UI Framework)",
+      "Authentication",
+      "Deployment for Development",
+    ],
+  },
+];
+export { workingHistory, educationHistory, nonFormalEducation };
