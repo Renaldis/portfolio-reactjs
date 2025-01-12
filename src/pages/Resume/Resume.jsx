bc92fd71e9e82b1d9bf6d2eaec8c8f2cc6925e22
Renaldis <renaldis142@gmail.com>
2025-01-12 17:39:01 +0700

    feat: making layout resume with grid columns

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,7 +1,17 @@
+import { workingHistory, educationHistory } from "../../utils/resumeDate";
+
 export default function Resume() {
   return (
     <section id="resume">
       <h1>Resume</h1>
+      <div className="grid grid-cols-2">
+        <div>
+          <h1>Working History</h1>
+        </div>
+        <div>
+          <h1>Working History</h1>
+        </div>
+      </div>
     </section>
   );
 }
---
+++ src/utils/resumeDate.js
@@ -0,0 +1,43 @@
+let workingHistory = [
+  {
+    title: "Art Director - Facebook Inc",
+    date: "2010 - present",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+  },
+  {
+    title: "Art Director - Google Inc",
+    date: "2010 - present",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+  },
+  {
+    title: "Art Director - Yahoo Inc",
+    date: "2010 - present",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+  },
+];
+
+let educationHistory = [
+  {
+    title: "Abc University of Los Angeles",
+    date: "2004 - 2009",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+  },
+  {
+    title: "Abc University of Los Angeles",
+    date: "2004 - 2009",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+  },
+  {
+    title: "Abc University of Los Angeles",
+    date: "2004 - 2009",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+  },
+];
+
+export { workingHistory, educationHistory };
