71867b4b282a1f69f2b9085786d97c6f779da8c5
Renaldis <renaldis142@gmail.com>
2025-01-12 18:50:36 +0700

    feat(resume): include experience and formal education

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,14 +2,92 @@ import { workingHistory, educationHistory } from "../../utils/resumeDate";
 
 export default function Resume() {
   return (
-    <section id="resume">
-      <h1>Resume</h1>
-      <div className="grid grid-cols-2">
-        <div>
-          <h1>Working History</h1>
-        </div>
-        <div>
-          <h1>Working History</h1>
+    <section
+      id="resume"
+      className="bg-white shadow-sm rounded-lg border border-slate-100"
+    >
+      <div className="p-5">
+        <h1 className="mb-5">Resume</h1>
+        <div className="grid grid-cols-2">
+          <div className="relative">
+            <ol className="left-5 relative border-s-2 border-gray-200 dark:border-gray-700">
+              <li className="mb-8 ms-4 flex items-center">
+                <div className="absolute -start-5 flex items-center justify-center w-10 h-10 bg-yellow-300 rounded-full border border-yellow-300 shadow-md">
+                  <i className="fas fa-shopping-bag" />
+                </div>
+                <span className="ms-3 font-semibold">Experience</span>
+              </li>
+              {workingHistory.map((item) => {
+                return (
+                  <li className="mb-2 ms-4 text-sm">
+                    <div className="absolute w-3 h-3 bg-yellow-200 rounded-full mt-1.5 -start-[7.6px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex items-center gap-2">
+                      <div className="overflow-hidden rounded-full w-14 bg-blue-600">
+                        <img
+                          src={item.img}
+                          alt={item.company}
+                          className="w-full"
+                        />
+                      </div>
+                      <h1 className="font-semibold">{item.title}</h1>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-slate-500">{item.date}</span>
+                      <span className="text-slate-500">
+                        {item.location} | {item.locType}
+                      </span>
+                    </div>
+                    <ul>
+                      {item.description.map((desc) => {
+                        return <li>- {desc}</li>;
+                      })}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ol>
+          </div>
+          <div className="relative">
+            <ol className="left-5 relative border-s-2 border-gray-200 dark:border-gray-700">
+              <li className="mb-8 ms-4 flex items-center">
+                <div className="absolute -start-5 flex items-center justify-center w-10 h-10 bg-yellow-300 rounded-full border border-yellow-300 shadow-md">
+                  <i className="fas fa-graduation-cap" />
+                </div>
+                <span className="ms-3 font-semibold">Formal Education</span>
+              </li>
+              {educationHistory.map((item) => {
+                return (
+                  <li className="mb-2 ms-4 text-sm">
+                    <div className="absolute w-3 h-3 bg-yellow-200 rounded-full mt-1.5 -start-[7.6px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex items-center gap-2">
+                      <div className="overflow-hidden rounded-full w-20">
+                        <img
+                          src={item.img}
+                          alt={item.title}
+                          className="w-full"
+                        />
+                      </div>
+                      <h1 className="font-semibold">{item.title}</h1>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-slate-500">{item.date}</span>
+                      <span className="text-slate-500">{item.location}</span>
+                    </div>
+                    <h1 className="text-slate-700">Learn:</h1>
+                    <ul className="list-disc ms-4">
+                      {item.description.map((desc) => {
+                        return (
+                          <div className="flex flex-col text-slate-800 cursor-pointer">
+                            <li>{desc.title}</li>
+                          </div>
+                        );
+                      })}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ol>
+          </div>
         </div>
       </div>
     </section>
--- src/utils/resumeDate.js
+++ src/utils/resumeDate.js
@@ -1,42 +1,72 @@
 let workingHistory = [
   {
-    title: "Art Director - Facebook Inc",
-    date: "2010 - present",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
-  },
-  {
-    title: "Art Director - Google Inc",
-    date: "2010 - present",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
-  },
-  {
-    title: "Art Director - Yahoo Inc",
-    date: "2010 - present",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+    title: "Course Assistant",
+    employmentType: "Contract",
+    company: "Lembaga Pengembangan Komputerisasi Universitas Gunadarma",
+    date: "Oct 2024 - present",
+    location: "Depok, West Java, Indonesia",
+    locType: "on-site",
+    description: [
+      "Helping instructor during courses",
+      "inputting the grades of practitioners who attended the course",
+    ],
+    img: "https://media.licdn.com/dms/image/v2/C510BAQFZuJ22U-5ECw/company-logo_100_100/company-logo_100_100/0/1630580476989?e=1744848000&v=beta&t=e5TKH8gQ0gDallZ0GpeImKav9TMorxwp64A0xmmHXkA",
   },
 ];
 
 let educationHistory = [
   {
-    title: "Abc University of Los Angeles",
-    date: "2004 - 2009",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
-  },
-  {
-    title: "Abc University of Los Angeles",
-    date: "2004 - 2009",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
-  },
-  {
-    title: "Abc University of Los Angeles",
-    date: "2004 - 2009",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Commodi, ut consequatur tenetur nostrum sapiente hic magnam esse provident, enim nam ipsa, distinctio est suscipit saepe odio ? Voluptates sed tempore sapiente.",
+    title: "Universitas Gunadarma",
+    date: "Sept 2022 - Sept 2026(expected)",
+    location: "Depok, West Java, Indonesia",
+    img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/08/logo_Universitas-Gunadarma-thumb.png",
+    description: [
+      {
+        id: 1,
+        title: "Fundamental Programming",
+        desc: "I studied the basic concepts of programming, such as algorithm logic, data structures, and program implementation using various programming languages.",
+      },
+      {
+        id: 2,
+        title: "Fundamental Web",
+        desc: "I learned the basics of web development, including creating web pages using HTML, CSS, and JavaScript.",
+      },
+      {
+        id: 3,
+        title: "Fundamental DBMS",
+        desc: "I studied database management systems, covering data management, database design, normalization, and integrity constraints.",
+      },
+      {
+        id: 4,
+        title: "Oracle Beginner & Intermediate",
+        desc: "I attended Oracle courses to understand database management using Oracle tools, including query creation, data management, and database optimization.",
+      },
+      {
+        id: 5,
+        title: "SQL Beginner & Intermediate:",
+        desc: "I explored Structured Query Language (SQL) to create, read, update, and delete data from databases, as well as learning advanced queries for data analysis.",
+      },
+      {
+        id: 6,
+        title: "C# Beginner & Intermediate",
+        desc: "I learned programming using C#, including the basics of Object-Oriented Programming (OOP), data manipulation, and developing simple applications.",
+      },
+      {
+        id: 7,
+        title: "HTML, CSS, JavaScript",
+        desc: "I studied techniques for creating interactive and responsive web pages, starting from basic structure (HTML), design (CSS), to interactive logic (JavaScript).",
+      },
+      {
+        id: 8,
+        title: "PHP for backend",
+        desc: "I learned to develop dynamic web-based applications using PHP, including database connections and server-side data processing.",
+      },
+      {
+        id: 9,
+        title: "UI/UX Design-Figma Prototyping",
+        desc: "I developed prototypes and wireframes for various projects using Figma, focusing on creating intuitive and functional designs.",
+      },
+    ],
   },
 ];
 
