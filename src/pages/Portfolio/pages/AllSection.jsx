85ea5dfd3bc9d32e44cc21a7aeda38b4d0556ca0
Renaldis <renaldis142@gmail.com>
2025-01-15 16:53:47 +0700

    update data tech php in portfolio.js

--- src/pages/Portfolio/pages/AllSection.jsx
+++ src/pages/Portfolio/pages/AllSection.jsx
@@ -4,11 +4,17 @@ import { dataContext } from "../../../components/context/dataContext";
 export default function AllSection() {
   const { projects } = useContext(dataContext);
   const [dataJS, setDataJS] = useState([]);
+  const [dataPHP, setDataPHP] = useState([]);
+
   useEffect(() => {
-    const filteredData = projects.filter(
+    const filteredDataJS = projects.filter(
       (project) => project.tech === "javascript"
     );
-    setDataJS(filteredData);
+    setDataJS(filteredDataJS);
+    const filteredDataPHP = projects.filter(
+      (project) => project.tech === "php"
+    );
+    setDataPHP(filteredDataPHP);
   }, [projects]);
 
   return (
@@ -54,7 +60,51 @@ export default function AllSection() {
             </div>
           ))
         ) : (
-          <p>No projects with JavaScript tech found.</p>
+          <p>Tidak ada projects tersedia.</p>
+        )}
+      </div>
+      <h1 className="font-semibold text-slate-800 mt-5 mb-2">PHP</h1>
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2">
+        {dataPHP.length > 0 ? (
+          dataPHP.map((project) => (
+            <div
+              key={project.id}
+              className="container-card w-auto border border-slate-200 rounded-md shadow-sm overflow-hidden"
+            >
+              <img
+                src={project.image_url}
+                alt={`${project.id} - ${project.title}`}
+                className="w-full"
+              />
+              <div className="px-2 py-1">
+                <h2>{project.title}</h2>
+                <p className="text-sm text-slate-600">
+                  {project.short_descript.length > 40
+                    ? project.short_descript.slice(0, 40)
+                    : project.short_descript}
+                  ...
+                </p>
+                <div className="flex justify-start gap-3 my-5">
+                  {project.link_project.trim() !== "" && (
+                    <button className="group text-sm border border-slate-400 px-2 py-1 text-white bg-yellow-300 rounded-full group-hover:bg-white group-hover:text-yellow-300">
+                      <a href={project.link_project} target="_blank">
+                        Visit Project
+                      </a>
+                    </button>
+                  )}
+                  {project.link_github.trim() !== "" && (
+                    <button className="group text-sm border border-yellow-200 px-2 py-1 rounded-full text-yellow-400 hover:bg-yellow-300 hover:text-white group-hover:bg-white group-hover:text-yellow-400">
+                      <a href={project.link_github} target="_blank">
+                        Visit Code
+                      </a>
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Tidak ada projects tersedia.</p>
         )}
       </div>
     </div>
--- src/utils/portfolio/portfolio.js
+++ src/utils/portfolio/portfolio.js
@@ -1,6 +1,6 @@
 const project = [
   {
-    id: "1",
+    id: 1,
     title: "SiniLoker.id",
     short_descript:
       "Website lowongan kerja berbasis ReactJS untuk mempermudah pencarian dan pengelolaan informasi pekerjaan.",
@@ -11,33 +11,40 @@ const project = [
     tech: "javascript",
   },
   {
-    id: "2",
-    title: "SiniLoker.com",
+    id: 2,
+    title: "GurmaQuizClone",
     short_descript:
-      "Website lowongan kerja berbasis ReactJS untuk mempermudah pencarian dan pengelolaan informasi pekerjaan.",
-    link_project: "https://renaldi.reactjssanbercode.my.id/",
-    link_github: "",
-    image_url: "../../../public/project/finalproject-reactjs.png",
+      "Website QuizClone berbasis JS untuk Melatih Logika Pemrograman NPM 11122233 password 123321.",
+    link_project: "https://gurmaquizclone.netlify.app/",
+    link_github: "https://github.com/Renaldis/gurmaquizclone/tree/main",
+    image_url: "../../../public/project/Clone-QuizApp-University.png",
     tech: "javascript",
   },
   {
-    id: "3",
-    title: "SiniLoker.com",
-    short_descript:
-      "Website lowongan kerja berbasis ReactJS untuk mempermudah pencarian dan pengelolaan informasi pekerjaan.",
-    link_project: "https://renaldi.reactjssanbercode.my.id/",
-    link_github: "",
-    image_url: "../../../public/project/finalproject-reactjs.png",
+    id: 3,
+    title: "Personal Website V1",
+    short_descript: "Website Portfolio Pribadi berbasis JS.",
+    link_project: "https://personalweb142.netlify.app/",
+    link_github: "https://github.com/Renaldis/PortfolioBeginner",
+    image_url: "../../../public/project/PersonalWebsite-V1.png",
     tech: "javascript",
   },
   {
-    id: "3",
-    title: "SiniLoker.com",
-    short_descript:
-      "Website lowongan kerja berbasis ReactJS untuk mempermudah pencarian dan pengelolaan informasi pekerjaan.",
-    link_project: "https://renaldi.reactjssanbercode.my.id/",
+    id: 4,
+    title: "Final Project Dicoding: Bloggsite",
+    short_descript: "Dicoding Dasar Pengembangan Web",
+    link_project: "https://bandungoverview.netlify.app/",
     link_github: "",
-    image_url: "../../../public/project/finalproject-reactjs.png",
+    image_url: "../../../public/project/Final-Project-Dicoding.png",
+    tech: "javascript",
+  },
+  {
+    id: 5,
+    title: "Digital Wedding RSVP: Sistem Pendaftaran Tamu Online",
+    short_descript: "Website undangan digital untuk konfirmasi RSVP",
+    link_project: "",
+    link_github: "https://github.com/Renaldis/Daftar-Tamu-Wedding",
+    image_url: "../../../public/project/daftar-tamu-wedding.png",
     tech: "php",
   },
 ];
